68e82b85f840c1be9be71c6d31c8eb1f71c88f64
NedduJS <nmamanipantoja@gmail.com>
2021-07-15 08:29:43 -0500

    Link with NextJS

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+
+import Navbar from '../components/Navbar/Navbar';
 
 const about = () => {
   return (
     <div>
+      <Navbar />
       <h1>Welcome to about section</h1>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+
+import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hello NextJS</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
