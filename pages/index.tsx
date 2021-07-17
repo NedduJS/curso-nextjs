4a28525a515994e1b87ba509a7613d7cf4534fda
NedduJS <nmamanipantoja@gmail.com>
2021-07-17 14:45:37 -0500

    Fetch own API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+  
+  useEffect(() => {
+    fetch('/api/avo')
+      .then(response => response.json())
+      .then(({ data, length }) => {
+        setProductList(data)
+      })
+  }, [])
+  
   return (
     <div>
       <Navbar />
       <h1>Hello NextJS</h1>
+      {productList.map(product => {
+        return <div key={product.id}>{ product.name}</div>
+      })}
     </div>
   );
 };
