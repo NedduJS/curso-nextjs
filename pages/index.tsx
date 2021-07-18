7112720f3436909e62498e01c997b307d7828098
NedduJS <nmamanipantoja@gmail.com>
2021-07-18 08:29:53 -0500

    Path alias

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
 
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from '@components/Navbar/Navbar';
 
 const Home = () => {
   const [productList, setProductList] = useState<TProduct[]>([])
